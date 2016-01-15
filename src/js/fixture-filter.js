b72bf6210867b3204fe52d52d01f7c3599395a33
jeumann <jeumann@ebay.com>
2016-01-15 12:31:37 +0100

    Created repo

---
+++ src/js/fixture-filter.js
@@ -0,0 +1,26 @@
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
+FixtureFinder.FixtureFilter = function(country, team){
+    var allCountries = 'all';
+    if(!country) country = allCountries;
+    if(!team) team = '';
+
+    var byCountry = function(fixture){
+        return fixture.country === country || country === allCountries;
+    };
+
+    team = team.trim();
+    var byTeam = function(fixture){
+        return fixture.homeTeam.toLowerCase().contains(team)
+               || fixture.awayTeam.contains(team);
+    };
+
+    var noCompetition = function(fixture){
+        return !fixture.competition.toLowerCase().contains('no_competition') &&
+               !fixture.competition.toLowerCase().contains('no competition') 
+    };
+
+    return function(fixtures) {
+       return fixtures.filter(byCountry).filter(byTeam).filter(noCompetition);
+    }
+};
---
+++ test/spec/fixture-filter-spec.js
@@ -0,0 +1,114 @@
+describe("A Fixture Filter", function() {
+  var testFixtures;
+  var filtered;
+  var allCountries = 'all';
+
+  beforeEach(function() {
+    testFixtures = testFixtureData;
+  });
+
+  describe("given undefined country and team", function() {
+    it("will return the identity for the given array", function() {
+        filter = FixtureFinder.FixtureFilter();
+
+        filtered = filter(testFixtures);
+
+        expect(filtered).toEqual(testFixtures);
+    });
+  });
+
+  describe("given all countries and a blank team", function() {
+    var filter;
+
+    it("will return the identity for the given array", function() {
+        filter = FixtureFinder.FixtureFilter(allCountries, "");
+
+        filtered = filter(testFixtures);
+
+        expect(filtered).toEqual(testFixtures);
+    });
+
+    it("will return the identity for the given array", function() {
+        filter = FixtureFinder.FixtureFilter(allCountries, "   ");
+
+        filtered = filter(testFixtures);
+
+        expect(filtered).toEqual(testFixtures);
+    });
+  });
+
+  describe("given 'Italy' as country filter and a blank team", function() {
+    var filter;
+
+    it("will return the subset with only Italian fixtures", function() {
+      filter = FixtureFinder.FixtureFilter("Italy", "");
+
+      filtered = filter(testFixtures);
+
+      expect(filtered).toEqual(testFixtures.slice(3, 5));
+    });
+  });
+
+  describe("given all countries filter", function() {
+
+    describe("and team Filter is 'A'", function() {
+        var filter;
+
+        it("will return the subset with only Teams with 'A' or 'a' (in this case all)", function() {
+            filter = FixtureFinder.FixtureFilter(allCountries, "A");
+
+            filtered = filter(testFixtures);
+
+            expect(filtered).toEqual(testFixtures);
+        });
+    });
+
+    describe("and team Filter is 'al'", function() {
+        var filter;
+
+        it("will return the subset with only Teams with 'Al' (case insensitive)", function() {
+            filter = FixtureFinder.FixtureFilter(allCountries, "Al");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0], testFixtures[2]];
+            expect(filtered).toEqual(expected);
+        });
+    });
+
+    describe("and team Filter is 'arsenal'", function() {
+         var filter;
+
+        it("will return the subset with only Teams with 'arsenal' (case insensitive)", function() {
+            filter = FixtureFinder.FixtureFilter(allCountries, "arsenal");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0]];
+            expect(filtered).toEqual(expected);
+        });
+
+        it("will return the subset with only Teams with 'ARSENAL' (case insensitive)", function() {
+            filter = FixtureFinder.FixtureFilter(allCountries, "ARsEnAL");
+
+            filtered = filter(testFixtures);
+
+            var expected = [ testFixtures[0]];
+            expect(filtered).toEqual(expected);
+        });
+    });
+
+    describe("and team Filter is 'zyzz'", function() {
+        var filter;
+
+        it("will return an empty array", function() {
+            filter = FixtureFinder.FixtureFilter(allCountries, "zyzz");
+
+            filtered = filter(testFixtures);
+
+            var expected = [];
+            expect(filtered).toEqual(expected);
+        });
+    });
+  });
+});
